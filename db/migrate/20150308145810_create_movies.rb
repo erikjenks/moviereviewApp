c1ae58e1aeeb0546661bf1618856eb68553c829f
Erik Jenks <erikjenks@gmail.com>
2015-03-08 11:17:20 -0400

    added users and associations

---
+++ db/migrate/20150308145810_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :description
+      t.string :movie_length
+      t.string :rating
+      t.string :director
+
+      t.timestamps null: false
+    end
+  end
+end
